1211c88b761b9a647dc85f9c1efe2c157713dff7
raj-edelta <raj.mangukiya@edeltaes.com>
2024-02-29 16:50:01 +0530

    changes

--- main.py
+++ main.py
@@ -232,4 +232,4 @@ def disconnect():
         print(f"Disconnect handler error: {e}. Ignoring disconnection.")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=7500, host='0.0.0.0')
+    socketio.run(app, debug=True, port=7500, host='0.0.0.0') 
